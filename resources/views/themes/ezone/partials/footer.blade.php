20536dfac62d8f74ab4f7b17ddfa4221c018190b
Dityap12 <teddyaditya69@gmail.com>
2022-02-04 11:42:15 +0800

    Update footer.blade.php

--- resources/views/themes/ezone/partials/footer.blade.php
+++ resources/views/themes/ezone/partials/footer.blade.php
@@ -6,7 +6,8 @@
                 <div class="footer-widget mb-30">
                     <a href="#"><img src="{{ asset('themes/ezone/assets/img/logo/2.png') }}" alt=""></a>
                     <div class="footer-about-2">
-                        <p>There are many variations of passages of Lorem Ipsum <br>the majority have suffered alteration in some form, by <br> injected humour</p>
+                        <p>selamat datang di ilookdaily <br>kami menjual pakaian wanita berkualitas dengan offline store berbasis di Bali, 
+                        <br> order yang masuk sebelum jam 3 sore akan diusahakan  diproses hari itu juga</p>
                     </div>
                 </div>
                 <div class="footer-widget mb-30">
@@ -17,7 +18,7 @@
                                 <span>Address: </span>
                             </div>
                             <div class="footer-info-content3">
-                                <p>66 Sipu road Rampura Banasree <br>USA- 10800</p>
+                                <p>Jl. Gatot Subroto Tengah, Tonja, Kec. <br>Denpasar Utara, Kota Denpasar, Bali 80233</p>
                             </div>
                         </div>
                         <div class="footer-address-furniture">
@@ -25,7 +26,7 @@
                                 <span>Phone: </span>
                             </div>
                             <div class="footer-info-content3">
-                                <p>+8801 (33) 515609735 <br>+8801 (66) 223352333</p>
+                                <p>+62 878 619 390 69 </p>
                             </div>
                         </div>
                         <div class="footer-address-furniture">
@@ -33,7 +34,7 @@
                                 <span>E-mail: </span>
                             </div>
                             <div class="footer-info-content3">
-                                <p><a href="#"> email@domain.com</a> <br><a href="#"> domain@mail.info</a></p>
+                                <p><a href="#"> mediariadiputu@gmail.com</a> <br><a href="#"> domain@mail.info</a></p>
                             </div>
                         </div>
                     </div>
@@ -62,7 +63,7 @@
             <div class="row">
                 <div class="col-12 text-center">
                     <div class="copyright-furniture">
-                        <p>Copyright © <a href="https://hastech.company/">HasTech</a> 2018 . All Right Reserved.</p>
+                        <p>Copyright © <a href="https://www.instagram.com/ilookdaily/">Ilookdaily</a> 2022 . All Right Reserved.</p>
                     </div>
                 </div>
             </div>
